b0c7f37a3a33a311468fe6409fc04ea1def79a80
pouria4AH <87314029+pouria4AH@users.noreply.github.com>
2022-02-23 01:02:03 +0330

    Add templite and Create ViewComponent for Heder and Footer

---
+++ MarketPlace.Web/ViewComponents/SiteFooterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.ViewComponents
+{
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+}
