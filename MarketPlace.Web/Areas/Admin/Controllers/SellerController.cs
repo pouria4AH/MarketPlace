73df0d7d17f28f994b4733adca88c61c3db1e363
pouria4AH <87314029+pouria4AH@users.noreply.github.com>
2022-03-17 16:12:16 +0330

    add requeset sellers tabel

---
+++ MarketPlace.Web/Areas/Admin/Controllers/SellerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MarketPlace.Application.Services.interfaces;
+using MarketPlace.DataLayer.DTOs.Seller;
+
+namespace MarketPlace.Web.Areas.Admin.Controllers
+{
+    public class SellerController : AdminBaseController
+    {
+        #region ctor
+
+        private readonly ISellerService _sellerService;
+        public SellerController(ISellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        #endregion
+
+        #region Seller requset
+
+        public async Task<IActionResult> SellerRequests(FilterSellerDTO filter)
+        {
+            return View(await _sellerService.FilterSellers(filter));
+        }
+
+        #endregion
+    }
+}
